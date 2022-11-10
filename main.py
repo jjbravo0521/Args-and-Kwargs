224ed32cc8e9438ff617d844cdb84fea554608bb
Juan Bravo <113551841+jjbravo0521@users.noreply.github.com>
2022-11-10 20:13:08 +0000

    sum

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-def my_sum(num1,num2):
-  print(num1,num2)
+# def my_sum(num1,num2):
+#   print(num1,num2)
 
-print(my_sum(3,4,5))
-def a_sum (*args):
+# print(my_sum(3,4,5))
+# def a_sum (*args):
 
-# args are for entering
-# as many argunents as poossible
-  for num in args:
-    print(num)
+# # args are for entering
+# # as many argunents as poossible
+#   for num in args:
+#     print(num)
 
-print(a_sum(1))
-print(a_sum(1,3))
-print(a_sum(1,3,4,5,6))
+# print(a_sum(1))
+# print(a_sum(1,3))
+# print(a_sum(1,3,4,5,6))
+list = [3,4,5,6,7,8,9,10,11,12]
+def sum_numbers(list):
+  for num in list:
+    sum = sum + num
+    return sum
+    print(sum(list))
+
+# args
+def a_sum(*arg):
+  total = 0 
+for num in args:
+  total += num
+  # return total
+  return sum(args)
+
+print(a_sum(3,4,5,6,7,8,9,10,11,12))
